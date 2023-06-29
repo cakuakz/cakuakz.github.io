7072cfc1586d013d7eea9770fdc93b3befce0003
cakuakz <rafiirvansyah88@gmail.com>
2023-06-30 02:25:29 +0700

    commit photo

--- src/component/ProjectCard.jsx
+++ src/component/ProjectCard.jsx
@@ -16,12 +16,12 @@ function ProjectCard() {
     const [dataProject] = React.useState(projectData)
 
     return (
-        <div className="grid grid-cols-3 mt-[89px] justify-items-center mb-[282px]">
+        <div className="grid grid-cols-3 mt-[103px] justify-items-center mb-[282px]">
         {dataProject.map((item, i) => (
         <div className="">    
-            <div className="bg-[#F7F9FE] saturate-0 px-[25px] py-[25px] rounded-[30px] transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 hover:saturate-100">
-                    <img src={item.image} alt="project1" className="w-[342px] h-[257px] rounded-[20px]"/>
-                    <div className="flex justify-between items-center mt-[32px]">
+            <div className="bg-[#F7F9FE] saturate-0 pb-[25px] rounded-[20px] transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 hover:saturate-100">
+                    <img src={item.image} alt="project1" className="w-[391px] h-[269px] rounded-t-[20px]"/>
+                    <div className="flex justify-between px-[24px] items-center mt-[32px]">
                         <h1 className="font-semibold text-[32px] w-[276px] flex-1">{item.title}</h1>
                         <span className="arrow-sided transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300" onClick={() => toggle(i)}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><g fill="currentColor"><path d="m14.829 11.948l1.414-1.414L12 6.29l-4.243 4.243l1.415 1.414L11 10.12v7.537h2V10.12l1.829 1.828Z"/><path fill-rule="evenodd" d="M19.778 4.222c-4.296-4.296-11.26-4.296-15.556 0c-4.296 4.296-4.296 11.26 0 15.556c4.296 4.296 11.26 4.296 15.556 0c4.296-4.296 4.296-11.26 0-15.556Zm-1.414 1.414A9 9 0 1 0 5.636 18.364A9 9 0 0 0 18.364 5.636Z" clip-rule="evenodd"/></g></svg>

d8f5e87e21c5313851c24a90fc23e9e3437fa914
cakuakz <rafiirvansyah88@gmail.com>
2023-06-30 21:15:00 +0700

    spacing

--- src/component/SkillLists.js
+++ src/component/SkillLists.js
@@ -2,7 +2,7 @@ import SkillCircle from "./SkillCircle";
 
 const SkillLists = ({ skills }) => {
     return ( 
-        <div className="grid lg:flex lg:flex-row justify-center space-y-10 lg:space-x-[50px]">
+        <div className="grid gap-y-8 lg:flex lg:flex-row justify-center lg:space-x-[50px]">
             <SkillCircle image={skills[0].image} className="py-[34px] pr-[34px] pl-[31.5px]"/>
             <SkillCircle image={skills[1].image} className="py-[34px] pr-[34px] pl-[31.5px]"/>
             <SkillCircle image={skills[2].image} className="py-[32px] pr-[34px] pl-[31.5px]"/>
--- src/section/Description.js
+++ src/section/Description.js
@@ -10,7 +10,7 @@ const Description = () => {
                     <h1 className="text-[#A8BCDB] text-center text-[64px] font-semibold">4</h1>
                     <p className="text-[#CDCCCB] pt-4 text-[22px]">Completed<br/> Projects</p>
                 </div>
-                <div className="flex justify-center xl:pl-[30px] space-x-[14px]">
+                <div className="flex justify-center xl:justify-start xl:pl-[30px] space-x-[14px]">
                     <h1 className="text-[#A8BCDB] text-center text-[64px] font-semibold">2</h1>
                     <p className="text-[#CDCCCB] pt-4 text-[22px] font-normal">Years of<br/> Experience</p>
                 </div>

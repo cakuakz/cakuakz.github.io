507761dc6d79c14d6334896e2d2a33c428f8452f
cakuakz <rafiirvansyah88@gmail.com>
2024-09-16 01:26:02 +0700

    feat: fixing projects responsiveness

--- src/section/AllPortfolio.js
+++ src/section/AllPortfolio.js
@@ -7,10 +7,10 @@ const AllPortfolio = () => {
     const [cardData] = useState(project)
 
     return ( 
-        <div className="flex flex-col mt-32 px-36 mb-10">
+        <div className="flex flex-col mt-32 lg:px-36 px-3 lg:mb-10 mb-6">
             <h1 className="text-5xl font-bold">Projects</h1>
             <p className="text-sm font-normal text-[#a59c95] mt-4">Showcase of my projects on software development</p>
-            <div className="grid grid-cols-3 gap-y-6 mt-6">
+            <div className="grid xl:grid-cols-3 gap-y-6 mt-6">
                 {cardData.map((cardItem) => (
                     <PortfolioCard 
                         title={cardItem.title}
